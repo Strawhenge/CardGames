f0d5913f5bf97c465b2bd040fea8ce4cb514c636
Matt Bristow <strawhengestudios@gmail.com>
2024-01-23 12:05:57 +0000

    added basic deck object

---
+++ CardGames.Core/Deck/Deck.cs
@@ -0,0 +1,16 @@
+﻿using CardGames.Core.Cards;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardGames.Core.Deck
+{
+    public class Deck
+    {
+        public Deck(IEnumerable<Card> cards)
+        {
+            Cards = cards.ToArray();
+        }
+
+        public IReadOnlyList<Card> Cards { get; }
+    }
+}
