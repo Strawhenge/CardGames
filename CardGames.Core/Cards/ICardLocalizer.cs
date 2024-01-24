2ec98288a1bf3ecdcc440a046e84675d0c33f22f
Matt Bristow <strawhengestudios@gmail.com>
2024-01-24 11:49:16 +0000

    tidy up

--- CardGames.Core/Cards/ICardLocalizer.cs
+++ CardGames.Core/Cards/ICardLocalizer.cs
@@ -4,21 +4,4 @@
     {
         string GetName(Card card);
     }
-
-    public class CardNames : ICardLocalizer
-    {
-        public string GetName(Card card)
-        {
-            if (card.Rank == Rank.Joker)
-                switch (card.Suit)
-                {
-                    case Suit.RedJoker:
-                        return "Red Joker";
-                    case Suit.BlackJoker:
-                        return "Black Joker";
-                }
-
-            return $"{card.Rank} of {card.Suit}";
-        }
-    }
 }
