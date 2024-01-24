94e8dc8720c827822f83f0e5a1ce7abcef3408cc
Matt Bristow <strawhengestudios@gmail.com>
2024-01-24 11:57:24 +0000

    added card comparison methods

--- CardGames.Core/Cards/Card.cs
+++ CardGames.Core/Cards/Card.cs
@@ -11,5 +11,9 @@
         public Rank Rank { get; }
 
         public Suit Suit { get; }
+
+        public bool IsSameRankAs(Card card) => card.Rank == Rank;
+
+        public bool IsSameSuitAs(Card card) => card.Suit == Suit;
     }
 }
