3602bc7bccb540d237929351fcffce220b68c27f
Matt Bristow <strawhengestudios@gmail.com>
2024-01-25 12:20:28 +0000

    added basic card playability check

---
+++ Shithead.Tests/CardPlayabilityTests.cs
@@ -0,0 +1,57 @@
+﻿using CardGames.Core.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Shithead.Tests
+{
+    public class CardPlayabilityTests
+    {
+        readonly Wastepile _wastepile;
+        readonly CardPlayability _cardplayability;
+
+        public CardPlayabilityTests()
+        {
+            _wastepile = new Wastepile();
+            _cardplayability = new CardPlayability(_wastepile);
+        }
+
+        [Fact]
+        public void Card_should_be_playable_when_wastepile_is_empty()
+        {
+            var card = Card.EightOfHearts;
+
+            Assert.True(
+                _cardplayability.CanPlay(card));
+        }
+
+        [Fact]
+        public void Card_with_same_rank_as_last_should_be_playable()
+        {
+            _wastepile.Add(
+               Card.FourOfClubs,
+               Card.FiveOfDiamonds);
+
+            var card = Card.FiveOfClubs;
+
+            Assert.True(
+                _cardplayability.CanPlay(card));
+        }
+
+        [Fact]
+        public void Card_lower_than_last_played_should_not_be_playable()
+        {
+            _wastepile.Add(
+                 Card.FourOfClubs,
+                 Card.SevenOfClubs);
+
+            var card = Card.FiveOfClubs;
+
+            Assert.False(
+                _cardplayability.CanPlay(card));
+        }
+    }
+}
---
+++ Shithead/WastepileExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CardGames.Core.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shithead
+{
+    public static class WastepileExtensions
+    {
+        public static void Add(this Wastepile wastepile, params Card[] cards) =>
+            wastepile.Add((IReadOnlyList<Card>)cards);
+
+        public static void Add(this Wastepile wastepile, IReadOnlyList<Card> cards)
+        {
+            foreach (Card card in cards)
+                wastepile.Add(card);
+        }
+    }
+}
