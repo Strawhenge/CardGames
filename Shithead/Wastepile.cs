3218896a84b2b01105d58ce3e0b2256e15fb8784
Matt Bristow <strawhengestudios@gmail.com>
2024-01-25 11:44:16 +0000

    added wastepile

---
+++ Shithead/Wastepile.cs
@@ -0,0 +1,21 @@
+﻿using CardGames.Core.Cards;
+using System.Collections.Generic;
+
+namespace Shithead
+{
+    public class Wastepile
+    {
+        readonly Stack<Card> _cards = new Stack<Card>();
+
+        public IReadOnlyList<Card> Cards => _cards.ToArray();
+
+        public void Add(Card card) => _cards.Push(card);
+
+        public IReadOnlyList<Card> Clear()
+        {
+            var cards = _cards.ToArray();
+            _cards.Clear();
+            return cards;
+        }
+    }
+}
