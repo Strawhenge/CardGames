e96da803838d5d003bb2a18d5152cd719a68de32
Matt Bristow <strawhengestudios@gmail.com>
2024-01-27 12:02:11 +0000

    implemented card order

---
+++ CardGames.Core/Cards/CardExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CardGames.Core.Cards.Order;
+
+namespace CardGames.Core.Cards
+{
+    public static class CardExtensions
+    {
+        public static bool IsRankHigherThan(this Card card, Card other, CardOrder order) =>
+            order.Compare(card, other) > 0;
+
+        public static bool IsRankHigherThanOrSameAs(this Card card, Card other, CardOrder order) =>
+            order.Compare(card, other) >= 0;
+
+        public static bool IsRankLowerThan(this Card card, Card other, CardOrder order) =>
+            order.Compare(card, other) < 0;
+
+        public static bool IsRankLowerThanOrSameAs(this Card card, Card other, CardOrder order) =>
+            order.Compare(card, other) <= 0;
+    }
+}
