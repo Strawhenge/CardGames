72fc9c97838b7a7eed5fd1fd4e47e45f9d8e59ec
Matt Bristow <strawhengestudios@gmail.com>
2024-02-08 19:13:13 +0000

    added arrange starting cards

---
+++ Shithead.Tests/ArrangeStartingCardsTests.cs
@@ -0,0 +1,68 @@
+﻿using CardGames.Core.Cards;
+using Shithead.CardCollections;
+using Xunit;
+
+namespace Shithead.Tests
+{
+    public class ArrangeStartingCardsTests
+    {
+        [Fact]
+        public void No_swapping_should_return_same_card_positions()
+        {
+            var startingCards = CreateStartingCards();
+
+            var arrange = new ArrangeStartingCards(startingCards);
+
+            var newStartingCards = arrange.Finish();
+
+            Assert.Multiple(
+                () => Assert.Same(startingCards.FaceUp.First, newStartingCards.FaceUp.First),
+                () => Assert.Same(startingCards.InHand.First, newStartingCards.InHand.First),
+                () => Assert.Same(startingCards.FaceUp.Second, newStartingCards.FaceUp.Second),
+                () => Assert.Same(startingCards.InHand.Second, newStartingCards.InHand.Second),
+                () => Assert.Same(startingCards.InHand.Third, newStartingCards.InHand.Third),
+                () => Assert.Same(startingCards.InHand.Third, newStartingCards.InHand.Third)
+                );
+        }
+
+        [Fact]
+        public void Swapping_face_down_cards_with_in_hand_cards_should_return_them_swapped()
+        {
+            var startingCards = CreateStartingCards();
+
+            var arrange = new ArrangeStartingCards(startingCards);
+
+            arrange.FirstFaceUp.SwapWithThirdInHand();
+            arrange.ThirdFaceUp.SwapWithSecondInHand();
+
+            var newStartingCards = arrange.Finish();
+
+            Assert.Multiple(
+                () =>
+                {
+                    Assert.Same(startingCards.FaceUp.First, newStartingCards.InHand.Third);
+                },
+                () =>
+                {
+                    Assert.Same(startingCards.InHand.Third, newStartingCards.FaceUp.First);
+                },
+                () =>
+                {
+                    Assert.Same(startingCards.FaceUp.Third, newStartingCards.InHand.Second);
+                },
+                () =>
+                {
+                    Assert.Same(startingCards.InHand.Second, newStartingCards.FaceUp.Third);
+                });
+        }
+
+        static StartingCards CreateStartingCards()
+        {
+            return new StartingCards(
+                faceDown: new ThreeCards(Card.AceOfClubs, Card.EightOfClubs, Card.FiveOfHearts),
+                faceUp: new ThreeCards(Card.SixOfClubs, Card.KingOfClubs, Card.NineOfClubs),
+                inHand: new ThreeCards(Card.FourOfSpades, Card.TenOfClubs, Card.AceOfClubs)
+                );
+        }
+    }
+}
---
+++ Shithead/ArrangeStartingCards.cs
@@ -0,0 +1,83 @@
+﻿using CardGames.Core.Cards;
+using Shithead.CardCollections;
+using System;
+
+namespace Shithead
+{
+    public class ArrangeStartingCards
+    {
+        readonly ThreeCards _faceDownCards;
+
+        Card _firstFaceUp;
+        Card _secondFaceUp;
+        Card _thirdFaceUp;
+        Card _firstInHand;
+        Card _secondInHand;
+        Card _thirdInHand;
+
+        public ArrangeStartingCards(StartingCards cards)
+        {
+            _faceDownCards = cards.FaceDown;
+
+            _firstFaceUp = cards.FaceUp.First;
+            _secondFaceUp = cards.FaceUp.Second;
+            _thirdFaceUp = cards.FaceUp.Third;
+            _firstInHand = cards.InHand.First;
+            _secondInHand = cards.InHand.Second;
+            _thirdInHand = cards.InHand.Third;
+
+            FirstFaceUp = new FaceUpCardActions(
+                swapWithFirstInHand: () => (_firstFaceUp, _firstInHand) = (_firstInHand, _firstFaceUp),
+                swapWithSecondInHand: () => (_firstFaceUp, _secondInHand) = (_secondInHand, _firstFaceUp),
+                swapWithThirdInHand: () => (_firstFaceUp, _thirdInHand) = (_thirdInHand, _firstFaceUp));
+
+            SecondFaceUp = new FaceUpCardActions(
+                swapWithFirstInHand: () => (_secondFaceUp, _firstInHand) = (_firstInHand, _secondFaceUp),
+                swapWithSecondInHand: () => (_secondFaceUp, _secondInHand) = (_secondInHand, _secondFaceUp),
+                swapWithThirdInHand: () => (_secondFaceUp, _thirdInHand) = (_thirdInHand, _secondFaceUp));
+
+            ThirdFaceUp = new FaceUpCardActions(
+                swapWithFirstInHand: () => (_thirdFaceUp, _firstInHand) = (_firstInHand, _thirdFaceUp),
+                swapWithSecondInHand: () => (_thirdFaceUp, _secondInHand) = (_secondInHand, _thirdFaceUp),
+                swapWithThirdInHand: () => (_thirdFaceUp, _thirdInHand) = (_thirdInHand, _thirdFaceUp));
+        }
+
+        public FaceUpCardActions FirstFaceUp { get; }
+
+        public FaceUpCardActions SecondFaceUp { get; }
+
+        public FaceUpCardActions ThirdFaceUp { get; }
+
+        public StartingCards Finish()
+        {
+            return new StartingCards(
+                _faceDownCards,
+                faceUp: new ThreeCards(_firstFaceUp, _secondFaceUp, _thirdFaceUp),
+                inHand: new ThreeCards(_firstInHand, _secondInHand, _thirdInHand)
+                );
+        }
+
+        public class FaceUpCardActions
+        {
+            readonly Action _swapWithFirstInHand;
+            readonly Action _swapWithSecondInHand;
+            readonly Action _swapWithThirdInHand;
+
+            internal FaceUpCardActions(
+                Action swapWithFirstInHand,
+                Action swapWithSecondInHand,
+                Action swapWithThirdInHand)
+            {
+                _swapWithFirstInHand = swapWithFirstInHand;
+                _swapWithSecondInHand = swapWithSecondInHand;
+                _swapWithThirdInHand = swapWithThirdInHand;
+            }
+
+            public void SwapWithFirstInHand() => _swapWithFirstInHand();
+
+            public void SwapWithSecondInHand() => _swapWithSecondInHand();
+
+            public void SwapWithThirdInHand() => _swapWithThirdInHand();
+        }
+    }
+}
