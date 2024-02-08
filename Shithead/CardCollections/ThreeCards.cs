0f33c445b28fa0661b8ffaa579bee6d41cb92c78
Matt Bristow <strawhengestudios@gmail.com>
2024-02-08 17:51:06 +0000

    added three cards object

---
+++ Shithead/CardCollections/ThreeCards.cs
@@ -0,0 +1,20 @@
+﻿using CardGames.Core.Cards;
+
+namespace Shithead.CardCollections
+{
+    public class ThreeCards
+    {
+        public ThreeCards(Card first, Card second, Card third)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+        }
+
+        public Card First { get; }
+        
+        public Card Second { get; }
+
+        public Card Third { get; }
+    }
+}
