066bdd6940d9a8799d3bb174198a5a65dd67d3f2
Matt Bristow <strawhengestudios@gmail.com>
2024-02-08 17:59:14 +0000

    added starting cards object

---
+++ Shithead/CardCollections/StartingCards.cs
@@ -0,0 +1,18 @@
+﻿namespace Shithead.CardCollections
+{
+    public class StartingCards
+    {
+        public StartingCards(ThreeCards faceDown, ThreeCards faceUp, ThreeCards inHand)
+        {
+            FaceDown = faceDown;
+            FaceUp = faceUp;
+            InHand = inHand;
+        }
+
+        public ThreeCards FaceDown { get; }
+
+        public ThreeCards FaceUp { get; }
+
+        public ThreeCards InHand { get; }
+    }
+}
